1a61ae9352c47a83840ad685ead8254082d1a63f
HiliCeShip <nghlc1707@gmail.com>
2026-04-21 11:18:40 +0700

    add epoch

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 class config:
     BATCH_SIZE = 16
     max_len = 128
-    epochs = 10
+    epochs = 12
     LEARNING_RATE = 2e-5
     MODEL_NAME = 'indobenchmark/indobert-base-p1'
     thresold = 0.5
